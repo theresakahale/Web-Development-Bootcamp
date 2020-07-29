5baac5535e0080a649aa0535fed529d8fc6c04bf
Theresa Kahale <48251154+theresakahale@users.noreply.github.com>
2020-07-29 13:03:11 +0300

    Add files via upload

---
+++ Session 10/Lyrics Search/script.js
@@ -0,0 +1,100 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+const apiURL = 'https://api.lyrics.ovh';
+
+
+//Search By song or artist
+async function searchSongs(term){
+const res = await fetch(`${apiURL}/suggest/${term}`);
+const data = await res.json();
+ showData(data);
+}
+
+//Show song and artist
+
+function showData(data){
+    // let output = '';
+    // data.data.forEach(song => {
+    //     output += `
+    //     <li>
+    //     <span><strong>${song.artist.name}
+    //     </strong> - ${song.title}</span>
+    //     <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    //     </li>
+    // `;
+    // });
+    // result.innerHTML= `
+    // <ul class="songs">
+    // ${output}
+    // </ul>
+    // `;
+
+    //SAME Target but another way
+
+    result.innerHTML = `
+    <ul class="songs">
+    ${data.data.map(song => ` <li>
+    <span><strong>${song.artist.name}
+    </strong> - ${song.title}</span>
+    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`)
+    .join('')
+    }
+    </ul>
+    `;
+
+    if(data.prev || data.next){
+        more.innerHTML = `
+        ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''
+    }
+        ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''
+    }`;
+    }else{
+        more.innerHTML = '';
+    }
+}
+
+//get prev and next results
+async function getMoreSongs(url) {
+    const a = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await a.json();
+    showData(data);
+}
+
+async function getLyrics(artist, songtitle){
+    const a = await fetch(`${apiURL}/v1/${artist}/${songtitle}`);
+
+    const data = await a.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songtitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
+
+// Event Listeners
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const searchTerm = search.value.trim();
+    if(!searchTerm){
+        alert('Please type in a search term');
+    } else{
+        searchSongs(searchTerm);
+    }
+});
+
+//Get Lyrics button click
+result.addEventListener('click', e=> {
+const clickedEl = e.target;
+if(clickedEl.tagName === 'BUTTON'){
+    const artist = clickedEl.getAttribute('data-artist');
+    const songtitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songtitle);
+}
+});
